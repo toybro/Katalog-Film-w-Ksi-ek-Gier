14cc311c2d75a6a69c1043052e3ca2e4ca9051c0
toybro <34341513+toybro@users.noreply.github.com>
2026-02-19 20:15:19 +0100

    Add files via upload

---
+++ tkinter do projektu.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Biblioteka Danych")
+root.geometry("800x600")
+
+label = tk.Label(root, text="Zasoby główne", font=("Arial", 26))
+label.pack(pady=50)
+
+#FUNKCJE PROGRAMH DO OKIEN
+
+def klikkataloggry():
+    nowe_okno = tk.Toplevel(root)
+    nowe_okno.title("Katalog Gier")
+    nowe_okno.geometry("400x300")
+
+    powrot = tk.Button(nowe_okno, text="Powrót", font=("Arial", 14), command=nowe_okno.destroy)
+    powrot.pack(pady=40)
+
+def klikkatalogfilmy():
+    nowe_okno = tk.Toplevel(root)
+    nowe_okno.title("Katalog Filmów")
+    nowe_okno.geometry("400x300")
+
+    powrot = tk.Button(nowe_okno, text="Powrót", font=("Arial", 14), command=nowe_okno.destroy)
+    powrot.pack(pady=40)
+
+def klikkatalogksiazki():
+    nowe_okno = tk.Toplevel(root)
+    nowe_okno.title("Katalog Książek")
+    nowe_okno.geometry("400x300")
+
+    powrot = tk.Button(nowe_okno, text="Powrót", font=("Arial", 14), command=nowe_okno.destroy)
+    powrot.pack(pady=40)
+
+#BUTONY
+
+button_frame = tk.Frame(root)
+button_frame.pack(pady=1)
+
+filmyklik = tk.Button(button_frame, text="Katalog filmów",
+                      command=klikkatalogfilmy, font=("Arial", 14))
+gryklik = tk.Button(button_frame, text="Katalog Gier",
+                    command=klikkataloggry, font=("Arial", 14))
+ksiazklik = tk.Button(button_frame, text="Katalog książek",
+                      command=klikkatalogksiazki, font=("Arial", 14))
+
+filmyklik.grid(row=0, column=0, padx=10, pady=3, sticky="w")
+gryklik.grid(row=1, column=0, padx=10, pady=3, sticky="w")
+ksiazklik.grid(row=2, column=0, padx=10, pady=3, sticky="w")
+
+root.mainloop()
